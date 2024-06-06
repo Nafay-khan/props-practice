b0e76565b681dd3b355fed39d74d40cbf0645e32
Nafay-Khan <nafaykhan422@mail.com>
2024-06-06 18:19:56 +0500

    list added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Card from "./comonents/Card"
+
+
+
+function App() {
+
+  return (
+    <>
+    <div className="flex flex-wrap gap-5">
+      <Card title="nike shoes" description="If a dog chews shoes whose shoes does he choose?"/>
+      <Card title="jordan shoes" description="If a dog chews shoes whose shoes does he choose?"/>
+      <Card title="puma shoes" description="If a dog chews shoes whose shoes does he choose?"/>
+      <Card title="hooka shoes" description="If a dog chews shoes whose shoes does he choose?"/>
+      <Card title="nikey shoes" description="If a dog chews shoes whose shoes does he choose?"/>
+      <Card title="new shoes" description="If a dog chews shoes whose shoes does he choose?"/>
+    </div>
+    </>
+  )
+}
+
+export default App
